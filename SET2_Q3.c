9d904cd99b6a2b7bdbfefc4ab404595bdd869661
SACHIN YADAV <contactsachin59@gmail.com>
2024-12-30 21:14:56 +0530

    Update SET2_Q3.c

--- SET2_Q3.c
+++ SET2_Q3.c
@@ -1,4 +1,4 @@
-//  Write a program to check whether a number is divisible by 97 or not. 
+// Q2 Write a program to check whether a number is divisible by 97 or not. 
 #include <stdio.h>
 
 int main(){
