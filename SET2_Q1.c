b9ab1fbcf33feee26b601cc1ad28ae7ea3174161
SACHIN YADAV <contactsachin59@gmail.com>
2024-12-30 21:13:51 +0530

    Update SET2_Q1.c

---
+++ SET2_Q1.c
@@ -0,0 +1,7 @@
+// Q1) Which of the following is invalid in C? 
+//     a. int a=1; int b = a; VALID
+//     b. int v = 3*3; VALID
+//     c. char dt = ‘21 dec 2020’;  INVALID (ONLY ONE CHARACTER CAN BE IN 'CHAR')
+
+
+// FOLLOW ME :)
