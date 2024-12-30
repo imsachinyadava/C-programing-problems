db6f003ac4ed57f5321707552eb1fe52ceb7adf8
SACHIN YADAV <contactsachin59@gmail.com>
2024-12-30 18:34:24 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -1,8 +1,9 @@
 /*Calculate the area of a circle and modify the same program to calculate the 
-volume of a cylinder given its radius and height. 
-#include <stdio.h>*/
+volume of a cylinder given its radius and height. */
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int radius , height;
     printf("Enter radius\n");
     scanf("%d", &radius);
