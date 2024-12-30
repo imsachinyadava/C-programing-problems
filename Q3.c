20a1e14e41d8226eb1078e97365af1bb799dc853
SACHIN YADAV <contactsachin59@gmail.com>
2024-12-30 18:36:38 +0530

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -6,6 +6,6 @@ int main(){
     float celcius;
     printf("Enter temperature in celcius\n");
     scanf("%f", &celcius);
-    printf("The temperature in Fahrenheit is %f", (celcius*9/5)+32);
+    printf("The temperature in Fahrenheit is %f", (celcius*9.0/5.0)+32);
     return 0;
 }
