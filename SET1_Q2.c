ccde3ba46dd6312be5093422b43ebea22d60e1d3
SACHIN YADAV <contactsachin59@gmail.com>
2025-01-01 20:48:55 +0530

    Update SET1_Q2.c

--- SET1_Q2.c
+++ SET1_Q2.c
@@ -1,5 +1,7 @@
-/*Calculate the area of a circle and modify the same program to calculate the 
-volume of a cylinder given its radius and height. */
+/*
+Calculate the area of a circle and modify the same program to calculate the 
+volume of a cylinder given its radius and height. 
+*/
 #include <stdio.h>
 
 int main()
