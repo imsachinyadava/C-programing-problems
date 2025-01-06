2c39e430cd5a030b75c91f76283111142e96fbc3
SACHIN YADAV <contactsachin59@gmail.com>
2025-01-06 18:21:24 +0530

    Update set5_Q1.c

--- set5_Q1.c
+++ set5_Q1.c
@@ -7,7 +7,8 @@ Q1. Write a program using function to find average of three numbers.
 int average(int ,  int , int);
 
 int average(int a , int b , int c){
-    printf("The average of the numbers is %d", (a+b+c)/3);
+    printf("The average of the numbers is");
+    return (a+b+c)/3
 }
 
 int main(){
